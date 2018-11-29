82e3355cd1a402ea09e56b0a217a69a868c9aed3
Gert Peter Rue Brigsted <manisto@manisto.dk>
2018-11-29 20:19:13 +0100

    Added basic tileset edit component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { SpritemapSettingsComponent } from "../spritemap-settings/spritemap-settings.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpritemapSettingsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/spritemap-settings/spritemap-settings.component.ts
@@ -0,0 +1,94 @@
+import { Component, ViewChild, ElementRef, AfterViewInit } from "@angular/core";
+
+interface TileSettings {
+    name: string;
+    collision: boolean;
+}
+
+@Component({
+    selector: "spritemap-settings",
+    templateUrl: "./spritemap-settings.html",
+})
+export class SpritemapSettingsComponent implements AfterViewInit {
+    @ViewChild("preview")
+    private previewReference: ElementRef<HTMLCanvasElement>;
+    private preview: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+    private spritemap: HTMLImageElement;
+    private margin: number;
+    private size: number;
+    private rows: number;
+    private columns: number;
+    private settings: TileSettings[];
+    private selectedTile: TileSettings;
+
+    fileSelected($event: Event): void {
+        let fileInput: HTMLInputElement = $event.target as HTMLInputElement;
+        let file = fileInput.files[0];
+        let url = URL.createObjectURL(file);
+        this.spritemap = new Image();
+        
+        this.spritemap.addEventListener("loadend", () => {
+            URL.revokeObjectURL(url);
+            this.initializeSettings();
+            this.updatePreview();
+        });
+
+        this.spritemap.src = url;
+        fileInput.value = null;
+    }
+
+    initializeSettings(): void {
+        this.rows = (this.spritemap.height + this.margin) / (this.size + this.margin);
+        this.columns = (this.spritemap.width + this.margin) / (this.size + this.margin);
+
+        this.settings = [];
+
+        for (let index = 0; index < this.rows * this.columns; index++) {
+            this.settings.push({
+                name: `Sprite${index + 1}`,
+                collision: false,
+            });
+        }
+
+        this.preview.width = this.spritemap.width * 2;
+        this.preview.height = this.spritemap.height * 2;
+
+        this.context.strokeStyle = "#F00";
+        this.context.scale(2, 2);
+        this.context.imageSmoothingEnabled = false;
+    }
+
+    updatePreview(): void {
+        if (!this.spritemap) {
+            return;
+        }
+
+        this.context.clearRect(0, 0, this.preview.width, this.preview.height);
+        this.context.globalAlpha = 1;
+        this.context.drawImage(this.spritemap, 0, 0);
+        this.context.globalAlpha = .5;
+        
+        for (let row = 0; row < this.rows; row++) {
+            for (let column = 0; column < this.columns; column++) {
+                let x = column * (this.size + this.margin);
+                let y = row * (this.size + this.margin);
+                this.context.strokeRect(x + .5, y + .5, this.size - 1, this.size - 1);
+            }
+        }
+    }
+
+    previewClicked($event: MouseEvent): void {
+        let x: number = $event.offsetX;
+        let y: number = $event.offsetY;
+        let row: number = Math.floor((y / 2) / (this.size + this.margin));
+        let column: number = Math.floor((x / 2) / (this.size + this.margin));
+        this.selectedTile = this.settings[(row * this.columns) + column];
+        console.log(this.settings[0]);
+    }
+
+    ngAfterViewInit(): void {
+        this.preview = this.previewReference.nativeElement;
+        this.context = this.preview.getContext("2d");
+    }
+}
